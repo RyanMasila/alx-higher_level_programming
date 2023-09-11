333ac36627fd86b74600dd67d1eb94d1588eb975
RyanMasila <ryanmasila12@gmail.com>
2023-09-11 23:17:27 +0300

    5. Geometry module

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+i#!/usr/bin/python3
+"""Module with class BaseGeometry"""
+
+class BaseGeometry
+    """Empty class"""
+
+    @classmethod
+    def area(self):
+        """Raises an exception"""
+        
+        raise (Exception("area() is not implemented"))
